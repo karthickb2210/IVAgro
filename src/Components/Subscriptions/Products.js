5b17ac428f6545a6e6b8b4a9815f2bda11311185
karthickb2210 <karthickb2210@gmail.com>
2024-10-10 22:04:36 +0530

    Product.js added

---
+++ src/Components/Subscriptions/Products.js
@@ -0,0 +1,44 @@
+const products = [
+    {
+      id: "m1",
+      name: "Baby Spinach",
+      description:
+        "Baby Spinach is high in vitamin K which serves several function in your body but it is best known for its role in blood clotting.",
+      image:
+        "https://t3.ftcdn.net/jpg/08/07/98/62/240_F_807986222_KjBaQUDhq5lvjb4FI3mU4Y7ViaHcTTeN.jpg",
+    },
+    {
+      id: "m5",
+      name: "Lettuce",
+      description:
+        "Lettuce is an excellent source of beta carotene (Vitamin A) which is needed for healthy skin, bones and eyes.",
+      image:
+        "https://t4.ftcdn.net/jpg/02/80/03/99/240_F_280039907_Ny5g14FK1JQU59POwyU5eJj8ZaQNjmQw.jpg",
+    },
+    {
+      id: "m7",
+      name: "Pak Choi",
+      description:
+        "It's full of cancer fighting compounds such as vitamin C and vitamin E, beta-carotene, folate and selenium.",
+      image:
+        "https://t3.ftcdn.net/jpg/03/92/37/26/240_F_392372698_06CR5RnRAQLHY7uzkqS9G6bfDeDPuuzk.jpg",
+    },
+    {
+      id: "m8",
+      name: "Kale",
+      description:
+        "It is a nutrition superstar due to the amounts of vitamin A, B6, C, K, folate, fiber, carotenoids, and manganese it contains.",
+      image:
+        "https://t3.ftcdn.net/jpg/02/30/97/16/240_F_230971608_hsf90qBGPcmUuv3m0LkWPAuI5i5PnQmL.jpg",
+    },
+    {
+      id: "m9",
+      name: "Basil",
+      description:
+        "It provides some macronutrients such as calcium and vitamin K, as well as a range of antioxidants.",
+      image:
+        "https://t3.ftcdn.net/jpg/01/48/71/76/240_F_148717694_VUiPGqDdJ6gOuHKDr6hAvAmI5qdpKZef.jpg",
+    },
+  ];
+
+  export default products;
