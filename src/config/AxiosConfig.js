e970a69e2440f83d7edb212b0c797b120c4fd8bf
karthickb2210 <karthickb2210@gmail.com>
2024-10-10 22:23:47 +0530

    axios change

--- src/config/AxiosConfig.js
+++ src/config/AxiosConfig.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/',
-  // baseURL : 'https://agronomy-9m1j.onrender.com/'
+  // baseURL: 'http://localhost:8080/',
+  baseURL : 'https://agronomy-9m1j.onrender.com/'
   // Replace with your backend URL
 });
 
