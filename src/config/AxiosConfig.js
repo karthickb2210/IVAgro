bf925a0b29f97c7bde1c8410e369ba1a6fb70a97
karthickb2210 <karthickb2210@gmail.com>
2024-10-14 19:06:36 +0530

    vite config changed for pwa

--- src/config/AxiosConfig.js
+++ src/config/AxiosConfig.js
@@ -2,9 +2,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'http://localhost:8080/',
+  baseURL: 'http://localhost:8080/',
   // baseURL : 'https://agronomy-9m1j.onrender.com/'
-   baseURL : "https://electronic-michell-ironvalleyagronomy-835a5573.koyeb.app/"
+  //  baseURL : "https://electronic-michell-ironvalleyagronomy-835a5573.koyeb.app/"
   // Replace with your backend URL
 });
 
